2e24315b79c77f34bd7e258a1b36ea62730e101f
MinhCanh <canhln1001@gmail.com>
2026-09-10 23:02:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/levuquy/',
+    base:'/vuquynhagai/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
